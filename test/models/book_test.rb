605868155988f25288f0cc3df6a80c62f0864229
Sam Rapaport <me@samrapdev.com>
2018-06-12 21:10:32 -0700

    Add validation to model

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "does not create a book without a title" do
+    book = Book.new
+    book.publish_date = '2018-10-09'
+
+    assert_not book.save
+  end
+
+  test "does not create a book without a publish date" do
+    book = Book.new
+    book.title = 'Sam\'s Big Adventure'
+
+    assert_not book.save
+  end
 end
